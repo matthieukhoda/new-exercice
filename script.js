58a4d229a207689c484e512b6dd18e26ee3f01a1
yakoub-75 <yakouboumarou@yahoo.com>
2020-09-14 15:43:19 +0200

    j'ai modifier ton fichier html et j'ai ajouter un fichier js

---
+++ script.js
@@ -0,0 +1,36 @@
+
+let numA = 10;
+let numB = 5;
+let result = divide(numA, numB);
+console.log(result);
+
+numA = 15;
+numB = 0;
+result = divide(numA, numB);
+console.log(result);
+
+result = divide(numB, numA);
+console.log(result);
+
+
+result = divide(numB, numA);
+console.log(result);
+
+
+function divide(a, b) {
+    if(b !== 0) {
+        return a/b;
+    }
+    return null;
+}
+
+result = divide(numB, numA);
+console.log(result);
+
+
+function divide(a, b) {
+    if(b !== 0) {
+        return a/b;
+    }
+    return null;
+}
